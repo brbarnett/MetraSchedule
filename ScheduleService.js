cc890dceb6ed4027dc8e948bf478a9a6984b8f10
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2017-11-18 16:23:32 -0600

    Ordering, formatting

--- ScheduleService.js
+++ ScheduleService.js
@@ -4,7 +4,6 @@ import data from './data.json';
 
 class ScheduleService {
     constructor() {
-
     }
 
     getFutureTrains(direction) {
@@ -12,12 +11,14 @@ class ScheduleService {
             const now = moment();
             const trains = _(data[direction])
                 .chain()
-                .map(x => ({
-                    id: x.id,
-                    eta: moment(x.eta, 'HH:mm'),
-                    etd: moment(x.etd, 'HH:mm'),
-                })
+                .map(x => (
+                    {
+                        id: x.id,
+                        eta: moment(x.eta, 'HH:mm'),
+                        etd: moment(x.etd, 'HH:mm'),
+                    })
                 )
+                .orderBy(x => x.etd)
                 .map(x => Object.assign(x, { duration: x.eta.diff(x.etd, 'minutes') }))
                 .map(x => Object.assign(x, { express: x.duration <= 45 }))
                 .filter(x => x.etd > now)
